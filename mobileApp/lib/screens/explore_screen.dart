20c5d3172659781a4959f2acf9b5aa1193957ae5
Kamil <kkuzmicki@wp.pl>
2023-01-19 15:27:13 +0100

    First set view tests (explore)

--- mobileApp/lib/screens/explore_screen.dart
+++ mobileApp/lib/screens/explore_screen.dart
@@ -11,10 +11,49 @@ class ExploreScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Explore')),
-      bottomNavigationBar: MenuBottom(),
-      drawer: MenuDrawer(),
-      body: Center(child: FlutterLogo())
-    );
+        appBar: AppBar(title: Text('Explore')),
+        bottomNavigationBar: MenuBottom(),
+        drawer: MenuDrawer(),
+        body: Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+              image: AssetImage('assets/sea.jpg'),
+              fit: BoxFit.cover,
+            )),
+            child: Center(
+                child: Container(
+              padding: const EdgeInsets.all(24),
+              child: ListView(children: [
+                Container(
+                    padding: const EdgeInsets.all(24),
+                    margin: EdgeInsets.only(bottom: 10),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white70,
+                    ),
+                    child: const Text('Start screen',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 22, shadows: [
+                          Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 2.0,
+                              color: Colors.grey)
+                        ]))),
+                Container(
+                    padding: const EdgeInsets.all(24),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white70,
+                    ),
+                    child: const Text('Start screen',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 22, shadows: [
+                          Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 2.0,
+                              color: Colors.grey)
+                        ])))
+              ]),
+            ))));
   }
 }
--- mobileApp/lib/screens/own_screen.dart
+++ mobileApp/lib/screens/own_screen.dart
@@ -14,36 +14,25 @@ class OwnScreen extends StatelessWidget {
         drawer: const MenuDrawer(),
         bottomNavigationBar: MenuBottom(),
         body: Container(
-          decoration: const BoxDecoration(
-            image: DecorationImage( 
+            decoration: const BoxDecoration(
+                image: DecorationImage(
               image: AssetImage('assets/sea.jpg'),
               fit: BoxFit.cover,
-            )
-          ),
-          child: Center(
-            child: Container(
-              padding: const EdgeInsets.all(24),
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(20)),
-                color: Colors.white70,
-              ),
-              child: const Text(
-                'Start screen',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 22,
-                  shadows: [
-                    Shadow(
-                      offset: Offset(1.0, 1.0),
-                      blurRadius: 2.0,
-                      color: Colors.grey
-                    )
-                  ]
-                )
-              )
-            )
-          )
-        )
-      );
+            )),
+            child: Center(
+                child: Container(
+                    padding: const EdgeInsets.all(24),
+                    decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      color: Colors.white70,
+                    ),
+                    child: const Text('Start screen',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(fontSize: 22, shadows: [
+                          Shadow(
+                              offset: Offset(1.0, 1.0),
+                              blurRadius: 2.0,
+                              color: Colors.grey)
+                        ]))))));
   }
 }
